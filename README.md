4614c6ab87e3f0add3689f6c29e519abf5b0bd7d
konrad babiarz <rundaer@gmail.com>
2020-07-22 14:00:31 +0200

    init update

--- README.md
+++ README.md
@@ -1,12 +1,28 @@
 # Kb_categorycustomfields
 
-1. Put all content inside x `modules/kb_categorycustomfields`.
-2. Go to install module.
-3. Clean cache in the admin area `Advanced Parameters / Performance`.
+### Requirements
 
-Warning: 
-- Prestashop copy the customer class in the [override folder](https://devdocs.prestashop.com/1.7/modules/concepts/overrides/#overriding-a-class).
+1. Composer, see [Composer](https://getcomposer.org/) to learn more
+
+### How to install
+
+1. Put all content inside `modules/kb_categorycustomfields`.'
+2. `cd` into module's directory and run following commands:
+    - `composer dumpautoload` to generate autoloader for module
+3. Go to install module.
+4. Clean cache in the admin area `Advanced Parameters / Performance`.
+
+### What it does
+
+Module adds extra custom field to category page
+
+### Warning
+
+If override class does not work, place it in root/override folder
+
+### Thanks to
 
 Thanks to
 - https://github.com/friends-of-presta/demo-cqrs-hooks-usage-module
 - https://github.com/frederic-benoist/fbsample-extracustomerfield
+- https://github.com/wfpaisa/prestashop-custom-field
--- kb_categorycustomfields.php
+++ kb_categorycustomfields.php
@@ -7,6 +7,9 @@ if (!defined('_PS_VERSION_')) {
 require_once __DIR__.'/vendor/autoload.php';
 
 use Prestashop\Module\Kb_CategoryCustomFields\Install\InstallerFactory;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use PrestaShopBundle\Form\Admin\Type\TranslatableType;
 
 class Kb_Categorycustomfields extends Module
 {
@@ -44,5 +47,52 @@ class Kb_Categorycustomfields extends Module
         $installer = InstallerFactory::create();
 
         return $installer->uninstall() && parent::uninstall();
+    }
+
+    public function hookActionCategoryFormBuilderModifier(array $params)
+    {
+        //Retrieving the form builder
+        /** @var FormBuilder $formBuilder */
+        $formBuilder = $params['form_builder'];
+ 
+        // https://devdocs.prestashop.com/1.7/development/components/form/types-reference/
+        $formBuilder->add(
+            'seo_text',
+            TranslatableType::class,
+            [
+                'label' => $this->l('Seo Text'),
+                'required' => false,
+                'type' => TextareaType::class
+            ]
+        );
+
+        // For more languages ??
+        // $languages = Language::getLanguages(true);
+        // foreach ($languages as $lang) {
+        //     $params['data']['seo_text'][$lang['id_lang']] = 'Custom value for lang '.$lang['iso_code'];
+        // }
+
+        $category = new Category($params['id']);
+        $params['data']['seo_text'] = $category->seo_text;
+
+        //Remember to put this line to update the data in the form
+        $formBuilder->setData($params['data']);
+    }
+
+    public function hookActionAfterCreateCategoryFormHandler(array $params)
+    {
+        $this->updateData($params['form_data'], $params);
+    }
+
+    public function hookActionAfterUpdateCategoryFormHandler(array $params)
+    {
+        $this->updateData($params['form_data'], $params);
+    }
+
+    protected function updateData(array $data, $params)
+    {
+        $cat = new Category((int)$params['id']);
+        $cat->seo_text = $data['seo_text'];
+        $cat->update();
     }
 }
